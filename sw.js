42f893b89f1d647ca5cfc2ad4341b32b7db90f2a
SergioCoP <20203tn125@utez.edu.mx>
2023-09-26 18:54:15 -0600

    [UPDATE] PWA PUSH AND SYNC

--- sw.js
+++ sw.js
@@ -10,5 +10,18 @@ self.addEventListener('activate',e=>{
 
 self.addEventListener('fetch',e=>{
     console.log(e.request);
-    e.respondWith(fetch(e.request));
+    if(e.request.url.includes('descarga.jpg')){
+        e.respondWith(fetch('img/fondo.jpg'))
+    }else{
+        e.respondWith(fetch(e.request))
+    }
+  
+})
+
+self.addEventListener('push',e=>{
+    console.log("Notificación push")
+})
+
+self.addEventListener('sync',e=>{
+    console.log("SYNC EVENT")
 })
