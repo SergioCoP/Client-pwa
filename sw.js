560c21e008d2adcdfaa4a20b25b9ffb6f266addb
Francisco Javier Garcia Espinoza <20203tn021@utez.edu.mx>
2023-09-26 18:41:35 -0600

    [UPDATE] Avance pwa y sw

--- sw.js
+++ sw.js
@@ -1,2 +1,14 @@
 console.log('SERVICEWORKWER');
 ///libreria de http-server npm i -g http-server
+self.addEventListener('install',e=>{
+    console.log("Instalado") 
+})
+
+self.addEventListener('activate',e=>{
+    console.log("Activado")
+})
+
+self.addEventListener('fetch',e=>{
+    console.log(e.request);
+    e.respondWith(fetch(e.request));
+})
