1cc7f5d714b917e3d38371eb944d939726d6faf4
SergioCoP <20203tn125@utez.edu.mx>
2023-10-02 18:53:09 -0600

    [ADD] Caches xd

--- js/app.js
+++ js/app.js
@@ -1,4 +1,15 @@
 console.log('APPJS')
+const url = window.location.href;
+const API = `http://localhost:3000/api`;
+let sWlocation = '/reportes/sw.js';
 if(navigator.serviceWorker){
-    navigator.serviceWorker.register('sw.js');
+    if(url.includes('localhost') || url.includes('127.0.0.1')){
+        sWlocation = '/sw.js';
+    }
+    window.addEventListener('load',()=>{
+        navigator.serviceWorker.register(sWlocation).then((reg)=>{
+            //Code
+        });
+    })
+    
 }
--- sw.js
+++ sw.js
@@ -1,7 +1,36 @@
 console.log('SERVICEWORKWER');
+
+const STATIC = 'staticv1';
+const STATIC_LIMIT = 15;
+const INMUTABLE = 'inmutablev1';
+const DYNAMIC = 'dynamic1';
+const DYNAMIC_LIMIT = 30;
+
+  //Todos los recursos propios de la aplicacion
+  const APP_SHELL = [
+    '/',
+    '/index.html',
+    'css/styles.css', 
+    'img/fondo.jpg',
+    'js/app.js'
+];
+//Todos aquellos recursos que nunca cambian
+const APP_SHELL_INMUTABLE = [
+    'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js',
+    'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css'
+];
+
 ///libreria de http-server npm i -g http-server
 self.addEventListener('install',e=>{
+    //e.skipWaiting();//se activa automaticamente el serviceworker
     console.log("Instalado") 
+    const staticCache =caches.open(STATIC).then(cache=>{
+        cache.addAll(APP_SHELL);
+    }) //se obtiene apartado de static en cache
+    const inmutableCache =caches.open(INMUTABLE).then(cache=>{
+        cache.addAll(APP_SHELL_INMUTABLE);
+    }) //se obtiene apartado de static en cache
+    e.waitUntil(Promise.all([staticCache,inmutableCache])); //esperar a que se guarden los recursos
 })
 
 self.addEventListener('activate',e=>{
@@ -9,14 +38,51 @@ self.addEventListener('activate',e=>{
 })
 
 self.addEventListener('fetch',e=>{
-    console.log(e.request);
-    if(e.request.url.includes('descarga.jpg')){
-        e.respondWith(fetch('img/fondo.jpg'))
-    }else{
-        e.respondWith(fetch(e.request))
-    }
-  
-})
+//1. Cache Only
+//e.respondWith(caches.match(e.request));//hace que url de request haga match con lo que hay en cache
+
+//2. Cache with network fallback
+// const source = caches.match(e.request).then(res=>{
+//     if(res) return res;
+//     return fetch(e.request).then(resFetch => {
+//         caches.open(DYNAMIC).then(cache=>{//en cache dinamico por no saber si es inmutable o no el cache
+//             cache.put(e.request,resFetch)
+//         });
+//         return resFetch.clone()//retornar la respuesta clonandola
+//     });
+// });
+// e.respondWith(source);
+
+//3. Network with cache fallback
+// const source = fetch(e.request).then(res=>{
+//     if(!res) throw Error("NotFound");
+//     //validar si el recurso ya existe en algun cache
+//     caches.open(DYNAMIC).then(cache=>{
+//         cache.put(e.request,res);//meter una validacion extra
+//     });
+//     return res.clone();
+// }).catch(err=>{
+//     return caches.match(e.request);
+// });
+// e.respondWith(source);
+
+
+//4.Cache with network update//aplicacion siempre actualizada
+//si no esta en cache lo agrega
+//para rendimiento critico, si el rendimiento es bajo.
+//Toda la aplicacion mesta un paso atras.
+//Siempre devuelve desde cache lo antiguo
+if(e.request.url.includes('bootstrap')){
+    return e.respondWith(caches.match(e.request));
+    const source = caches.open(STATIC).then(cache=>{
+        fetch(e.request).then(res=>{
+            cache.put(e.request,res);
+        });
+        return cache.match(e.request);
+    });
+    e.respondWith(source);
+}
+});
 
 self.addEventListener('push',e=>{
     console.log("Notificación push")
